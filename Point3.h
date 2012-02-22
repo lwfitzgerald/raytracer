45557c3fc065adcea45030e710272aa37055e5d1
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2012-02-22 17:40:23 +0000

    Rename Point -> Point3

--- Point3.h
+++ Point3.h
@@ -10,53 +10,53 @@
 
 #include "Vector3.h"
 
-class Point {
+class Point3 {
 public:
     double x;
     double y;
     double z;
 
-    __inline Point() {}
+    __inline Point3() {}
 
-    __inline Point(const double x, const double y, const double z) {
+    __inline Point3(const double x, const double y, const double z) {
         this->x = x;
         this->y = y;
         this->z = z;
     }
 
-    __inline Point(const Point& old) {
+    __inline Point3(const Point3& old) {
         this->x = old.x;
         this->y = old.y;
         this->z = old.z;
     }
 
-    __inline ~Point() {}
+    __inline ~Point3() {}
 
-    __inline Point operator-() {
-        return Point(
+    __inline Point3 operator-() {
+        return Point3(
             -this->x,
             -this->y,
             -this->z
         );
     }
 
-    __inline Point operator+(const Vector3& other) {
-        return Point(
+    __inline Point3 operator+(const Vector3& other) {
+        return Point3(
             this->x + other.x,
             this->y + other.y,
             this->z + other.z
         );
     }
 
-    __inline Point operator-(const Vector3& rhs) {
-        return Point(
+    __inline Point3 operator-(const Vector3& rhs) {
+        return Point3(
             this->x - rhs.x,
             this->y - rhs.y,
             this->z - rhs.z
         );
     }
 
-    __inline Vector3 operator-(const Point& rhs) {
+    __inline Vector3 operator-(const Point3& rhs) {
         return Vector3(
             this->x - rhs.x,
             this->y - rhs.y,
@@ -64,7 +64,7 @@ public:
         );
     }
 
-    __inline Point& operator=(const Point& rhs) {
+    __inline Point3& operator=(const Point3& rhs) {
         this->x = rhs.x;
         this->y = rhs.y;
         this->z = rhs.z;
@@ -72,7 +72,7 @@ public:
         return *this;
     }
 
-    __inline Point& operator+=(const Vector3& rhs) {
+    __inline Point3& operator+=(const Vector3& rhs) {
         this->x += rhs.x;
         this->y += rhs.y;
         this->z += rhs.z;
@@ -80,7 +80,7 @@ public:
         return *this;
     }
 
-    __inline Point& operator-=(const Vector3& rhs) {
+    __inline Point3& operator-=(const Vector3& rhs) {
         this->x -= rhs.x;
         this->y -= rhs.y;
         this->z -= rhs.z;
