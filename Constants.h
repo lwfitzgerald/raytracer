eec63d769816d6785e1b2484f4c578f0f212b06c
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2012-02-28 17:02:30 +0000

    Add constants

---
+++ Constants.h
@@ -0,0 +1,23 @@
+/*
+ * Constants.h
+ *
+ *  Created on: 28 Feb 2012
+ *      Author: darkip
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+// Render settings
+#define HORIZONTAL_RES 1
+#define VERTICAL_RES   1
+#define PIXEL_SIZE     1.0
+#define GAMMA          1.0
+
+// Define some colours
+#define BLACK          Colour(0, 0, 0)
+#define RED            Colour(1.0, 0, 0)
+#define GREEN          Colour(0, 1.0, 0)
+#define BLUE           Colour(0, 0, 1.0)
+
+#endif /* CONSTANTS_H_ */
